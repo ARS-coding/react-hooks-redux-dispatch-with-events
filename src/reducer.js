97e016c30d9567d01c7b12ba719336395f736d35
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-14 20:24:29 +0300

    Write the reducer for the counter.

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,11 @@
-// add code snippets from README
+let state;
+
+function counterReducer(state = { count: 0 }, action) {
+    console.log("reducer")
+    switch(action.type) {
+        case "counter/increment":
+            return { count: state.count + 1 }
+        default: 
+            return state;
+    };
+};
